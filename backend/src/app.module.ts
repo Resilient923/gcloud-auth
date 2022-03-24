afb34827bbbabf77207be6ae9e413e0bb7bc12c0
shinkyuchul <64014651+Resilient923@users.noreply.github.com>
2022-03-24 13:19:21 +0900

    add app.yaml

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthCheckModule } from './healthcheck/healthcheck.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [HealthCheckModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
