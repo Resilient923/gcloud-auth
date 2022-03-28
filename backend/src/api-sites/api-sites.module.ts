2a1b6326fef51746d63bbac1dfba0ab7881e7187
shinkyuchul <64014651+Resilient923@users.noreply.github.com>
2022-03-28 15:15:23 +0900

    fix: delete api-sites

--- backend/src/api-sites/api-sites.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ApiSiteController } from './api-sites.controller';
-import { ApiSiteService } from './api-site.service';
-@Module({
-  imports: [],
-  controllers: [ApiSiteController],
-  providers: [ApiSiteService],
-})
-export class ApiSiteModule {}
