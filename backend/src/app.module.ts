5ce54b8012edd1a8c489809d255aadbe157d31ef
shinkyuchul <64014651+Resilient923@users.noreply.github.com>
2022-06-30 10:36:15 +0900

    hotfix: squash

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { FirestoreModule } from './firestore/firestore.module';
+import { FirestoreModule } from './core/firestore/firestore.module';
 import { HealthCheckModule } from './healthcheck/healthcheck.module';
-import { ApiSiteModule } from './api-sites/api-sites.module';
+import { ApiSiteModule } from './sites/sites.module';
 
 @Module({
   imports: [
@@ -14,14 +14,15 @@ import { ApiSiteModule } from './api-sites/api-sites.module';
     FirestoreModule.forRoot({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        //keyFilename: configService.get<string>('SA_KEY') -> .json 파일 들어와야한다.
-        keyFilename: configService.get<string>('KEY_FILE_NAME'),
-        projectId: configService.get<string>('PROJECT_ID'),
+        // keyFilename: configService.get<string>('SA_KEY'), //-> .json 파일 들어와야한다.
+        //  keyFilename: configService.get<string>('KEY_FILE_NAME'),
+        //  projectId: configService.get<string>('PROJECT_ID'),
       }),
       inject: [ConfigService],
     }),
     HealthCheckModule, 
     ApiSiteModule
+    
   ],
   controllers: [AppController],
   providers: [AppService],
